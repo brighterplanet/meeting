d7a8bc4c1b1b03310e4eb3a6b84155c7280b6cd9
Ian Hough <ian@brighterplanet.com>
2011-02-23 18:03:54 -0800

    Add scope

--- lib/meeting.rb
+++ lib/meeting.rb
@@ -3,5 +3,6 @@ require 'emitter'
 module BrighterPlanet
   module Meeting
     extend BrighterPlanet::Emitter
+    scope 'The meeting emission estimate is the anthropogenic emissions from meeting building energy use. It includes CO2 emissions from direct fuel combustion and indirect fuel combustion to generate electricity.'
   end
 end
