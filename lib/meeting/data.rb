182b86a81395f6d700b4fde2133920f90f7f25ec
Seamus Abshere <seamus@abshere.net>
2011-06-09 00:05:27 -0500

    make tests green with new sniff

--- lib/meeting/data.rb
+++ lib/meeting/data.rb
@@ -2,15 +2,11 @@ module BrighterPlanet
   module Meeting
     module Data
       def self.included(base)
-        base.data_miner do
-          schema do
-            float  'area'
-            float  'duration'
-            string 'zip_code_name'
-            string 'state_postal_abbreviation'
-          end
-          
-          process :run_data_miner_on_belongs_to_associations
+        base.create_table do
+          float  'area'
+          float  'duration'
+          string 'zip_code_name'
+          string 'state_postal_abbreviation'
         end
       end
     end
